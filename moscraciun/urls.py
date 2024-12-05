da6272c1ec6da162442d628d1dab0c016bf971b2
Ion Riza <ionriza97@gmail.com>
2024-12-05 20:09:36 +0200

    change become_santa url parameter

--- moscraciun/urls.py
+++ moscraciun/urls.py
@@ -5,5 +5,5 @@ from moscraciun.views import home, become_santa
 
 urlpatterns = [
     path('', home, name='home'),
-    path('become-santa/<int:letter_id>/', become_santa, name='become_santa'),
+    path('become-santa/<int:wishing_id>/', become_santa, name='become_santa'),
 ]
